07e28ca9c2179b90c9756754464ff57bd45a39b5
bazil ahmed <bazil ahmed>
2017-01-11 16:29:44 +0530

    initial commit 2

--- app/src/main/java/com/influential/bazil/ieeeinstantmessenger/MainActivity.java
+++ app/src/main/java/com/influential/bazil/ieeeinstantmessenger/MainActivity.java
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        messageEditText= (EditText) findViewById(R.id.editText);
-        subjectEditText= (EditText) findViewById(R.id.editText2);
+        messageEditText = (EditText) findViewById(R.id.editText);
+        subjectEditText = (EditText) findViewById(R.id.editText2);
 
     }
 
